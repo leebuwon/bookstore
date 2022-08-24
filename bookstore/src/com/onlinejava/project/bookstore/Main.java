b52dcf5b8bc40b14b91a9f504656af1e919b9407
leebuwon <bw1611@naver.com>
2022-08-24 18:50:47 +0900

    first commit

---
+++ bookstore/src/com/onlinejava/project/bookstore/Main.java
@@ -0,0 +1,11 @@
+package com.onlinejava.project.bookstore;
+
+public class Main {
+    public static void main(String[] args) {
+        bookstore bookstore = new bookstore();
+        bookstore.pagescreen();
+        
+    }
+
+
+}
---
+++ bookstore/src/com/onlinejava/project/bookstore/bookstore.java
@@ -0,0 +1,25 @@
+package com.onlinejava.project.bookstore;
+
+import java.sql.SQLOutput;
+
+public class bookstore {
+    public void pagescreen() {
+        System.out.println("=================================================");
+        System.out.println("=                                               =");
+        System.out.println("=                                               =");
+        System.out.println("=           --------------------------          =");
+        System.out.println("=           |    Welcome bookstore   |          =");
+        System.out.println("=           |                        |          =");
+        System.out.println("=           |    1. Print book list  |          =");
+        System.out.println("=           |    2. Book Search      |          =");
+        System.out.println("=           |    0. Quit             |          =");
+        System.out.println("=           --------------------------          =");
+        System.out.println("=                                               =");
+        System.out.println("=                                               =");
+        System.out.println("=================================================");
+        System.out.println("Type the number of the command you want to run: ");
+        System.out.println("search keyword: ");
+
+
+    }
+}
